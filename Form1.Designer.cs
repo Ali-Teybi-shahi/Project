9d9f67559272c9d7293b5b4ff5d826b9b3781558
Ali teybishahi <aliteybi07@gmail.com>
2025-01-17 10:02:49 +0330

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Quiz_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.choicesGroupBox = new System.Windows.Forms.GroupBox();
+            this.choiceRadioButton4 = new System.Windows.Forms.RadioButton();
+            this.choiceRadioButton3 = new System.Windows.Forms.RadioButton();
+            this.choiceRadioButton2 = new System.Windows.Forms.RadioButton();
+            this.choiceRadioButton1 = new System.Windows.Forms.RadioButton();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.retryButton = new System.Windows.Forms.Button();
+            this.choicesGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.questionLabel.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.questionLabel.Location = new System.Drawing.Point(17, 21);
+            this.questionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(57, 28);
+            this.questionLabel.TabIndex = 0;
+            this.questionLabel.Text = "سوال";
+            // 
+            // choicesGroupBox
+            // 
+            this.choicesGroupBox.BackColor = System.Drawing.Color.Transparent;
+            this.choicesGroupBox.Controls.Add(this.choiceRadioButton4);
+            this.choicesGroupBox.Controls.Add(this.choiceRadioButton3);
+            this.choicesGroupBox.Controls.Add(this.choiceRadioButton2);
+            this.choicesGroupBox.Controls.Add(this.choiceRadioButton1);
+            this.choicesGroupBox.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.choicesGroupBox.Location = new System.Drawing.Point(17, 64);
+            this.choicesGroupBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.choicesGroupBox.Name = "choicesGroupBox";
+            this.choicesGroupBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.choicesGroupBox.Size = new System.Drawing.Size(776, 161);
+            this.choicesGroupBox.TabIndex = 1;
+            this.choicesGroupBox.TabStop = false;
+            this.choicesGroupBox.Text = "سوال";
+            // 
+            // choiceRadioButton4
+            // 
+            this.choiceRadioButton4.AutoSize = true;
+            this.choiceRadioButton4.Location = new System.Drawing.Point(23, 128);
+            this.choiceRadioButton4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.choiceRadioButton4.Name = "choiceRadioButton4";
+            this.choiceRadioButton4.Size = new System.Drawing.Size(83, 27);
+            this.choiceRadioButton4.TabIndex = 3;
+            this.choiceRadioButton4.TabStop = true;
+            this.choiceRadioButton4.Text = "Choice";
+            this.choiceRadioButton4.UseVisualStyleBackColor = true;
+            // 
+            // choiceRadioButton3
+            // 
+            this.choiceRadioButton3.AutoSize = true;
+            this.choiceRadioButton3.Location = new System.Drawing.Point(23, 96);
+            this.choiceRadioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.choiceRadioButton3.Name = "choiceRadioButton3";
+            this.choiceRadioButton3.Size = new System.Drawing.Size(83, 27);
+            this.choiceRadioButton3.TabIndex = 2;
+            this.choiceRadioButton3.TabStop = true;
+            this.choiceRadioButton3.Text = "Choice";
+            this.choiceRadioButton3.UseVisualStyleBackColor = true;
+            // 
+            // choiceRadioButton2
+            // 
+            this.choiceRadioButton2.AutoSize = true;
+            this.choiceRadioButton2.Location = new System.Drawing.Point(23, 64);
+            this.choiceRadioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.choiceRadioButton2.Name = "choiceRadioButton2";
+            this.choiceRadioButton2.Size = new System.Drawing.Size(83, 27);
+            this.choiceRadioButton2.TabIndex = 1;
+            this.choiceRadioButton2.TabStop = true;
+            this.choiceRadioButton2.Text = "Choice";
+            this.choiceRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // choiceRadioButton1
+            // 
+            this.choiceRadioButton1.AutoSize = true;
+            this.choiceRadioButton1.Location = new System.Drawing.Point(23, 32);
+            this.choiceRadioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.choiceRadioButton1.Name = "choiceRadioButton1";
+            this.choiceRadioButton1.Size = new System.Drawing.Size(83, 27);
+            this.choiceRadioButton1.TabIndex = 0;
+            this.choiceRadioButton1.TabStop = true;
+            this.choiceRadioButton1.Text = "Choice";
+            this.choiceRadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.nextButton.Location = new System.Drawing.Point(639, 261);
+            this.nextButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(85, 32);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.Text = "بعدی";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.scoreLabel.Location = new System.Drawing.Point(35, 301);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(56, 28);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "نمره :";
+            this.scoreLabel.Visible = false;
+            // 
+            // retryButton
+            // 
+            this.retryButton.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.retryButton.Location = new System.Drawing.Point(506, 301);
+            this.retryButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.retryButton.Name = "retryButton";
+            this.retryButton.Size = new System.Drawing.Size(218, 32);
+            this.retryButton.TabIndex = 4;
+            this.retryButton.Text = "دوباره امتحان کنید";
+            this.retryButton.UseVisualStyleBackColor = true;
+            this.retryButton.Visible = false;
+            this.retryButton.Click += new System.EventHandler(this.retryButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(815, 368);
+            this.Controls.Add(this.retryButton);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.choicesGroupBox);
+            this.Controls.Add(this.questionLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quiz Application";
+            this.choicesGroupBox.ResumeLayout(false);
+            this.choicesGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.RadioButton choiceRadioButton1;
+        private System.Windows.Forms.RadioButton choiceRadioButton2;
+        private System.Windows.Forms.RadioButton choiceRadioButton3;
+        private System.Windows.Forms.RadioButton choiceRadioButton4;
+        private System.Windows.Forms.GroupBox choicesGroupBox;
+        private System.Windows.Forms.Button retryButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_Game
+{
+    public partial class Form1 : Form
+    {
+        private List<Question> questions;
+        private int currentQuestionIndex;
+        private int score;
+        private bool quizFinished;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeQuiz();
+        }
+
+        private void InitializeQuiz()
+        {
+            questions = new List<Question>
+            {
+                new Question("کدام گزینه برای تعریف یک آرایه صحیح در سی شارپ درست است؟", new List<string> { "int array = new int[5]", "int[] int array = new int[5]", "array int[5]", "int array[5]" }, "int[] int array = new int[5]"),
+                new Question("نگه داری؟ false یا true چه نوعی از داده می تواند یک مقدار", new List<string> { "int", "float", "bool", "string" }, "bool"),
+                new Question("در سی شارپ چیست؟ int مقدار پیش فرض یک متغییر نوع", new List<string> { "1", "-1", "0", "مقدار پیش فرض ندارد" }, "0"),
+                new Question("برای جلوگیری از ارث بری از یک کلاس از چه کلمه ای استفاده می شود؟", new List<string> { "sealed", "abstract", "private", "virtual" }, "sealed"),
+                new Question("کدارم یک کلمه کلیدی بریا تعریف یک رابط در سی شاپ است؟", new List<string> { "interface", "abstract", "implements", "class" }, "interface"),
+                new Question("برای مدیریت خطاها در سی شاپ از چه ساختاری استفاده می کنیم؟", new List<string> { "if - else", "try - catch", "switch - case", "for - loop" }, "try - catch"),
+                new Question("کدام گزینه برای مقداردهی متغییر ثابت صحیح است؟", new List<string> { "int coset x = int 10", "int coset x = 10", "const int x; x = 10", "const int = 10" }, "const int = 10"),
+                new Question("کدام یک از کلمات کلیدی برای اجرای چندریسمانی استفاده می شود؟", new List<string> { "thread", "Task", "await", "همه موارد" }, "همه موارد"),
+                new Question("کدام ساختار برای استفاده از فضای نام صحیح است؟", new List<string> { "namespace System", "using System", "import System", "include System" }, "using System"),
+                new Question("کدام گزینه یک ویژگی از شی گرایی نیست؟", new List<string> { "کپسوله سازی", "ارث بری", "چندریسمانی", "چندریختی" }, "چندریسمانی"),
+                // Add more questions here
+            };
+
+            currentQuestionIndex = 0;
+            score = 0;
+            quizFinished = false;
+
+            DisplayQuestion();
+        }
+
+        private void DisplayQuestion()
+        {
+            if (currentQuestionIndex < questions.Count)
+            {
+                Question currentQuestion = questions[currentQuestionIndex];
+
+                questionLabel.Text = currentQuestion.Text;
+                choiceRadioButton1.Text = currentQuestion.Choices[0];
+                choiceRadioButton2.Text = currentQuestion.Choices[1];
+                choiceRadioButton3.Text = currentQuestion.Choices[2];
+                choiceRadioButton4.Text = currentQuestion.Choices[3];
+
+                choicesGroupBox.Controls.OfType<RadioButton>().ToList().ForEach(radioButton => radioButton.Checked = false);
+
+                choicesGroupBox.Visible = true;
+                nextButton.Visible = true;
+                scoreLabel.Visible = false;
+                retryButton.Visible = false;
+            }
+            else
+            {
+                quizFinished = true;
+                questionLabel.Text = "پایان آزمون";
+                choicesGroupBox.Visible = false;
+                nextButton.Visible = false;
+                scoreLabel.Visible = true;
+                retryButton.Visible = true;
+
+                scoreLabel.Text = $"نمره: {score}/{questions.Count}";
+            }
+        }
+
+
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            // Check the selected answer
+            if (choicesGroupBox.Controls.OfType<RadioButton>().Any(radioButton => radioButton.Checked))
+            {
+                Question currentQuestion = questions[currentQuestionIndex];
+                RadioButton selectedRadioButton = choicesGroupBox.Controls.OfType<RadioButton>().FirstOrDefault(radioButton => radioButton.Checked);
+                string selectedAnswer = selectedRadioButton.Text;
+
+                if (selectedAnswer == currentQuestion.CorrectAnswer)
+                {
+                    score++;
+                }
+
+                currentQuestionIndex++;
+                DisplayQuestion();
+            }
+            else
+            {
+                MessageBox.Show("لطفا یک گزینه را انتخاب کنید", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public class Question
+        {
+            public string Text { get; }
+            public List<string> Choices { get; }
+            public string CorrectAnswer { get; }
+
+            public Question(string text, List<string> choices, string correctAnswer)
+            {
+                Text = text;
+                Choices = choices;
+                CorrectAnswer = correctAnswer;
+            }
+        }
+
+        private void retryButton_Click(object sender, EventArgs e)
+        {
+            InitializeQuiz();
+        }
+    }
+}
